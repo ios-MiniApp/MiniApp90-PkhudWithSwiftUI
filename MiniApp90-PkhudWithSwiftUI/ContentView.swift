29de3feed92bd536a75d2f4b1f43ea7be0f0307d
Kota <maedakoutaa@gmail.com>
2022-09-05 12:23:05 +0900

    Initial Commit

---
+++ MiniApp90-PkhudWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp90-PkhudWithSwiftUI
+//
+//  Created by 前田航汰 on 2022/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp90-PkhudWithSwiftUI/MiniApp90_PkhudWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp90_PkhudWithSwiftUIApp.swift
+//  MiniApp90-PkhudWithSwiftUI
+//
+//  Created by 前田航汰 on 2022/09/05.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp90_PkhudWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
