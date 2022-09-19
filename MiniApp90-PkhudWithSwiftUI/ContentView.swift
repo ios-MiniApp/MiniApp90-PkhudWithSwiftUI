cfaa7ba4989c2fdb6acdcc92a1c0a2800484492c
Kota <maedakoutaa@gmail.com>
2022-09-19 21:52:07 +0900

    commit

--- MiniApp90-PkhudWithSwiftUI/ContentView.swift
+++ MiniApp90-PkhudWithSwiftUI/ContentView.swift
@@ -26,20 +26,6 @@ struct ContentView: View {
         }
         .PKHUD(isPresented: $PkhudProgress, HUDContent: .labeledProgress(title: "通信中", subtitle: "通信しています"), delay: .infinity)
         .PKHUD(isPresented: $PkhudSuccess, HUDContent: .labeledSuccess(title: "成功", subtitle: "通信に成功しました"), delay: 1.5)
-        /*
-         以下サンプル
-                .PKHUD(isPresented: $progress, HUDContent: .progress, delay: 1.0)
-                .PKHUD(isPresented: $systemActivity, HUDContent: .systemActivity, delay: 1.0)
-                .PKHUD(isPresented: $success, HUDContent: .success, delay: 1.0)
-                .PKHUD(isPresented: $error, HUDContent: .error, delay: 1.0)
-                .PKHUD(isPresented: $label, HUDContent: .label("ラベル"), delay: 1.0)
-                .PKHUD(isPresented: $image, HUDContent: .image(UIImage(named: "sample")), delay: 1.0)
-                .PKHUD(isPresented: $labeledProgress, HUDContent: .labeledProgress(title: "読み込み中", subtitle: "読み込み中です"), delay: 1.0)
-                .PKHUD(isPresented: $labeledSuccess, HUDContent: .labeledSuccess(title: "成功", subtitle: "成功しました"), delay: 1.0)
-                .PKHUD(isPresented: $labeledError, HUDContent: .labeledError(title: "失敗", subtitle: "失敗しました"), delay: 1.0)
-                .PKHUD(isPresented: $labeledImage, HUDContent: .labeledImage(image: UIImage(named: "sample"), title: "画像ラベル", subtitle: "画像ラベルです"), delay: 1.0)
-                .PKHUD(isPresented: $labeledRotatingImage, HUDContent: .labeledRotatingImage(image: UIImage(named: "sample"), title: "回転画像ラベル", subtitle: "回転画像ラベルです"), delay: 1.0)
-         */
     }
 }
 
